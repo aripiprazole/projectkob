64bd1ffa31716ce405001e368e0d5ef913804c56
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-11-09 02:39:54 -0300

    refactor(web): remove useless import and remove use AuthService in LoginPage

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -1,18 +1,15 @@
-import React, { useContext } from "react";
+import React from "react";
 
 import { useRouter } from "next/router";
-
-import { AuthService } from "~/services";
 
 import { Container, LoginButton } from "./styles";
 
 const LoginPage: React.VFC = () => {
   const router = useRouter();
-  const authService = useContext(AuthService.Context);
 
   return (
     <Container>
-      <LoginButton onClick={() => router.push(authService.authenticationLink)}>
+      <LoginButton onClick={() => router.push("authenticationLink")}>
         Login with Github
       </LoginButton>
     </Container>
